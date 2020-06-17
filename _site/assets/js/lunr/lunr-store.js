75eae4491805ad294f48c506201831fa6d6083f6
spindrifting <51013371+spindrifting@users.noreply.github.com>
2020-06-17 13:52:20 -0700

    Upload _site (was not sync'ing)

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Coming Soon!",
+        "excerpt":"Stay tuned for more information about the 2021 International Conference on Functional Imaging and Modeling of the Heart !  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/coming-soon/",
+        "teaser": null
+      },{
+        "title": "Conference Dates Announced!",
+        "excerpt":"FIMH-2021 will be held from June 21-24 (2021) and will be hosted on the Stanford University campus. Please note that we are planning to host two pre-conference workshops on June 21, 2021. The main program would start on June 22, 2021 and continue through mid-day on June 24, 2021. More...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/conference-dates/",
+        "teaser": null
+      }]
